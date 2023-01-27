e2c74bc41348a840343ecfda86ad2092ab9127eb
Sam Mangham <mangham@gmail.com>
2023-01-27 15:49:32 +0000

    Make `temp_conversion.py` worse
    Necessary as this repo carries through to the `project_management` project.

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,23 +1,10 @@
-"""A library to perform temperature conversions"""
+# Todo: Code is a bit unclear
 
-def fahr_to_celsius(fahr):
-    """Convert Fahrenheit to Celsius.
+def Fc(x):
+    Y = (x - 32) * (5 / 9)
+    return Y
 
-    Uses standard Fahrenheit to Celsius formula
-
-    Arguments:
-    fahr -- the temperature in Fahrenheit
-    """
-    celsius = ((fahr - 32) * (5/9)) 
-    return celsius
-
-def fahr_to_kelvin(fahr):
-    """Convert Fahrenheight to Kelvin.
-
-    Uses standard Fahrenheit to Kelvin formula
-
-    Arguments:
-    fahr -- the temperature in Fahrenheit
-    """
-    kelvin = fahr_to_celsius(fahr) + 273.15
-    return kelvin
+def FK(x):
+    y = Fc(x)
+    z = y + 273.15
+    return z
